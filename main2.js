a401e26321c426a30535cad42aeb84d12c8bdb87
Mzeo-Mariami <mzeomariami981@gmail.com>
2023-05-28 16:22:36 +0400

    final

--- main2.js
+++ main2.js
@@ -1,50 +1,89 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-};
-
-function setInputError(inputElement, message){
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-};
-
-function clearInputError(inputElement){
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
+function registerUser(event) {
+    event.preventDefault();
+  
+    const form = document.getElementById('registrationForm');
+    const userName = form.userName.value;
+    const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    const email = form.email.value;
+    const privateNumber = form.privateNumber.value;
+    const password = form.password.value;
+  
+    const userData = {
+        userName: userName,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        privateNumber: privateNumber,
+        password: password,
+        active: true
+    };
+    fetch('https://kketelauri-001-site1.gtempurl.com/api/user/adduser', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+          "userName": userData.userName,
+      "firstName": userData.firstName,
+      "lastName": userData.lastName,
+      "email": userData.email,
+      "privateNumber": userData.privateNumber,
+      "password": userData.password,
+      "active": true})
+  })
+     .then(response => response.json())
+  
+    .then(response => { console.log(JSON.stringify(response));
+        alert('User registered successfully!');
+        form.reset();
+    })
+    .catch(error => {
+        console.error(error);
+        alert('An error occurred during registration.');
     });
+  }
+  
+  
+  function loginUser(event) {
+    event.preventDefault();
+  
+    const form = document.getElementById('loginForm');
+    const email = form.loginEmail.value;
+    const password = form.loginPassword.value;
+    fetch('https://kketelauri-001-site1.gtempurl.com/api/user/login', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+         
+      "email": email,
+      
+      "password": password,
+      })
+  })
+     .then(response => response.json())
+  
+    .then(response => { console.log(JSON.stringify(response));
+        alert('Good job');
+        form.reset();
+        window.location.href = 'index.html';
+    })
+    .catch(error => {
+        console.error(error);
+        alert('An error occurred during registration.');
+    });
+  }
+  
+  
+  const registrationForm = document.getElementById('registrationForm');
+  registrationForm.addEventListener('submit', registerUser);
+  
+  
+  const loginForm = document.getElementById('loginForm');
+  loginForm.addEventListener('submit', loginUser);
+  
 
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e=>{
-        e.preventDefault();
-    
-    
-        setFormMessage(loginForm,"error", "Invaild username/password combination");
-   });
-   document.querySelectorAll(".form__input").forEach(inputElement =>{
-    inputElement.addEventListener("blur", e => {
-     if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length<10) {
-        setInputError(inputElement, "Username must be at least 10 characters in length");
-     }
-    });
-    inputElement.addEventListener("input", e => {
-        clearInputError(inputElement);
-    });
-   });
-});
